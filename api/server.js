517589013180c3d50cdc980b4428af6a813bed27
lephong94 <101884884+lephong94@users.noreply.github.com>
2023-03-04 17:28:11 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -10,6 +10,42 @@ server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
+
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query)
+})
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now();
+    req.body.updatedAt = Date.now();
+  }else if (req.method === 'PATCH' || req.method === 'PUT'){
+    req.body.updatedAt = Date.now();
+  }
+  next()
+})
+router.render = (req, res) => {
+  const headers = res.getHeaders();
+  const totalCount = headers['x-total-count'];
+  if(req.originalMethod === 'GET' && totalCount){
+    const queryParams = queryString.parse(req._parsedOriginalUrl.query);
+    const result = {
+      data: res.locals.data,
+      pagination:{
+        _page: Number.parseInt(queryParams._page) || 1,
+        _limit: Number.parseInt(queryParams._limit) || 10,
+        _totalRows: Number.parseInt(totalCount)
+      }
+    }
+  return res.jsonp(result)
+  }
+  res.jsonp(res.locals.data)
+}
+
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
